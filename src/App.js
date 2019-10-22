91ac2c7d2d094218e68f20af378f74670fb30c96
sethukumar0001 <boylooksrock@gmail.com>
2019-10-22 15:57:42 +0530

    basic

--- src/App.js
+++ src/App.js
@@ -15,21 +15,31 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.setState({ data: [{ name: 'raju', age: 19 }, { name: 'ramu', age: 26 }, { name: 'ravan', age: 25 }, { name: 'laxman', age: 30 }] })
+    this.setState({ data: [{ name: 'raju', age: 20 }, { name: 'ramu', age: 21 }, { name: 'ravan', age: 25 }, { name: 'laxman', age: 30 }] })
   }
   render() {
     return (
       <div>
-        <ul>
-          {this.state.data.map(item => {
-            return <li>{item.name}-{item.age}</li>;
-          })}
-        </ul>
-        Age Between 18 to 25:
-        Yes:<input type="checkbox" defaultChecked={((this.state.data.filter((data) => { return console.log(data.age >= 18 && data.age <= 25) })) === true) ? true : false} />
-        No:<input type="checkbox" defaultChecked={((this.state.data.filter((data) => { return console.log(data.age <= 18 && data.age >= 25) })) === true) ? true : false} />
-          {/* i used console.log for what data function returned its not related to condition*/}
-      </div>
+      <ul>
+        {
+          this.state.data.map((person,index) => {
+            let checked = null;
+
+            if(person.age >= 18 && person.age <= 25){
+              checked = true;
+            }
+
+          console.log("checked"+checked);
+          return(
+
+            <input type="checkbox" checked = {checked} key = {index} />
+          )
+
+        })
+        }
+      </ul>
+    </div>
+ 
     );
   }
 }
